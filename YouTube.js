82eade334d872147bd75ff1eb9e44b9ea674947c
Tachyon <taxyovio@cantab.net>
2022-06-18 19:51:12 +0100

    Prettier YouTube.js

--- YouTube.js
+++ YouTube.js
@@ -1,11 +1,11 @@
 if ($request.url.indexOf("&oad") != -1) {
-	$done({ response: {body: ""} });
+	$done({ response: { body: "" } })
 } else if ($request.url.indexOf("&ctier") != -1) {
-	let url = $request.url.replace(/ctier=[A-Z]/, "ctier=A");
-	$done({ response: { status: 302, headers: { Location: url } } });
+	let url = $request.url.replace(/ctier=[A-Z]/, "ctier=A")
+	$done({ response: { status: 302, headers: { Location: url } } })
 } else if ($request.url.indexOf("/ctier") != -1) {
-	let url = $request.url.replace(/ctier\/[A-Z]/, "ctier/A");
-	$done({ response: { status: 302, headers: { Location: url } } });
+	let url = $request.url.replace(/ctier\/[A-Z]/, "ctier/A")
+	$done({ response: { status: 302, headers: { Location: url } } })
 } else {
 	$done({})
 }
